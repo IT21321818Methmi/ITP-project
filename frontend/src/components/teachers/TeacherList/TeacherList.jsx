b96f409754dca7d22649e52907e61b0261d1dfb2
IT21321818Methmi <IT21321818@my.sliit.lk>
2023-05-04 23:58:11 +0530

    changed

--- frontend/src/components/teachers/TeacherList/TeacherList.jsx
+++ frontend/src/components/teachers/TeacherList/TeacherList.jsx
@@ -31,11 +31,13 @@ let TeacherList = () => {
             }
         }
         fetchTeachers();
-    }, []);
+    }, [state]);
         
         
 
-    let {loading, teachers, errorMessage} = state;
+    let {teachers} = state;
+
+
   
 
     return (
@@ -76,71 +78,73 @@ let TeacherList = () => {
     </section>
 
     <section className="teacher-list">
-          <div className="container">
-            <div className="row">
-                
-              <div className="col-md-6">
-                <div className="card">
-                    <div className="card-body">
-                        <div className="row align-items-center d-flex justify-content-around">
-                            <div className="col-md-4">
-                              <img
-                                src="https://icon-library.com/images/user-png-icon/user-png-icon-16.jpg"
-                                alt=""
-                                className="teacher-img"
-                              />
-                            </div>
-                            <div className="col-md-7">
-                              <ul className="list-group">
-                                <li className="list-group-item list-group-item-action">
-                                  Name:
-                                  <span className="fw-bold">
-                                    Rajan 
-                                  </span>
-                                </li>
-
-                                <li className="list-group-item list-group-item-action">
-                                  Mobile Number:
-                                  <span className="fw-bold">
-                                    0703783537
-                                  </span>
-                                </li>
-
-                                <li className="list-group-item list-group-item-action">
-                                  Email:
-                                  <span className="fw-bold">
-                                    rajan@gmail.com
-                                  </span>
-                                </li>
-                              </ul>
-                            </div>
-                            <div className="col-md-1 d-flex flex-column align-items-center">
-                              <Link
-                                to={`/teachers/view/teacherId`}
-                                className="btn btn-warning my-1"
-                              >
-                                <i className="fa fa-eye" />
-                              </Link>
-
-                              <Link
-                                to={`/teachers/edit/teacherId`}
-                                className="btn btn-primary my-1"
-                              >
-                                <i className="fa fa-pen" />
-                              </Link>
-
-                              <button className="btn btn-danger my-1">
-                                <i className="fa fa-trash" />
-                              </button>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
+  <div className="container">
+    <div className="row">
+      {teachers.length > 0 &&
+        teachers.map((teacher) => {
+          return (
+            <div className="col-md-6" key={teacher._id}>
+              <div className="card my-2">
+                <div className="card-body">
+                  <div className="row align-items-center d-flex justify-content-around">
+                    <div className="col-md-4">
+                      <img
+                        src={teacher.photo}
+                        alt=""
+                        className="teacher-img"
+                      />
                     </div>
-              
+                    <div className="col-md-7">
+                      <ul className="list-group">
+                        <li className="list-group-item list-group-item-action">
+                          Name:
+                          <span className="fw-bold">
+                            {teacher.name}
+                          </span>
+                        </li>
+
+                        <li className="list-group-item list-group-item-action">
+                          Mobile Number:
+                          <span className="fw-bold">
+                            {teacher.mobile}
+                          </span>
+                        </li>
+
+                        <li className="list-group-item list-group-item-action">
+                          Email:
+                          <span className="fw-bold">
+                            {teacher.email}
+                          </span>
+                        </li>
+                      </ul>
+                    </div>
+                    
+                    <div className="col-md-1 d-flex flex-column align-items-center">
+                    <Link to={`/teachers/view/${teacher.id}`}
+                    className="btn btn-warning my-1">
+                    <i className="fa fa-eye" />
+                    </Link>
+
+                    <Link to={`/teachers/edit/${teacher.id}`}
+                    className="btn btn-primary my-1">
+                    <i className="fa fa-pen" />
+                    </Link>
+
+                    <button className="btn btn-danger my-1">
+                    <i className="fa fa-trash" />
+                    </button>
+                    </div>
+
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-        </section>
+          );
+        })}
+    </div>
+  </div>
+</section>
+
   </React.Fragment>
  );
 };
